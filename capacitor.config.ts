9c7b0cbd4f6e3ae403d9da2129f9ad3acdfb61b5
Ezequiel Montero Ramirez <ezequiel@MacBook-Air-de-Ezequiel.local>
2025-11-05 20:15:39 +0100

    Versión inicial del proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SorteoCoche',
+  webDir: 'www'
+};
+
+export default config;
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import {
+  IonContent,
+  IonButton,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [
+    IonContent,
+    IonButton,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+  ],
+})
+export class HomePage {
+  constructor() {}
+}
